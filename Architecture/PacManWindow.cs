390defc6d08f0b72009da35bdcb9732f2ece4f00
Dmitry <hacker.dmitry@gmail.com>
2019-02-10 17:06:49 +0500

    Вертим членом

--- Architecture/PacManWindow.cs
+++ Architecture/PacManWindow.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -6,10 +7,34 @@ namespace PacMan
     public class PacManWindow : Form
     {
         Bitmap bitmap = new Bitmap(Image.FromFile("../../Pictures/field1.png"));
+        int x, y;
+        Graphics g;
+
+        public PacManWindow()
+        {
+            Timer timer = new Timer
+            {
+                Interval = 32,
+                Enabled = true
+            };
+            timer.Tick += TimerTick;
+        }
         
         protected override void OnPaint(PaintEventArgs e)
         {
-            e.Graphics.DrawImage(bitmap, new Point(0, 0));
+            if (g == null) g = e.Graphics;
+            Text = x.ToString();
+            base.OnPaint(e);  
+            e.Graphics.DrawImage(bitmap, new Point(x++, y++));
+            e.Graphics.ResetTransform();
+        }
+
+        void TimerTick(object sender, EventArgs args)
+        {
+            Text = x++.ToString();
+            Invalidate();
+//            g?.DrawImage(bitmap, new Point(x++, y++));
+//            g?.ResetTransform();
         }
     }
 }
